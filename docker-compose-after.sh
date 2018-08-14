dc200123d27fbcb7a08cfbdf647462eb75d7d22c
Jeremy Turowetz <j.turowetz@gmail.com>
2018-08-14 13:23:01 -0400

    add to setup scripts

--- docker-compose-after.sh
+++ docker-compose-after.sh
@@ -3,9 +3,11 @@
 echo 'Installing wordpress'
 docker-compose run --rm wp-cli wp core install --url=wordpress.develop --title=SomethingCool --admin_user=supervisor --admin_password=password --admin_email=info@example.com
 
+echo 'activate test theme'
+docker-compose run --rm wp-cli wp theme activate my-theme
+
 # echo 'Updateing database'
 # docker-compose run --rm wp-cli wp search-replace https://$OLDURL http://$DEVURL
 
-echo 'Activating plugins'
-docker-compose run --rm wp-cli wp plugin activate --all
-
+# echo 'Activating plugins'
+# docker-compose run --rm wp-cli wp plugin activate --all
