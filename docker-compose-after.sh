de8c0df9c9c6cb51276f8e7252252eea55f418a0
Jeremy Turowetz <j.turowetz@gmail.com>
2018-08-24 16:09:37 -0400

    add example functions for interacting with wpcli container

--- docker-compose-after.sh
+++ docker-compose-after.sh
@@ -1,13 +1,22 @@
 #! bin/bash
 
-echo 'Installing wordpress'
-docker-compose run --rm wp-cli wp core install --url=wordpress.develop --title=SomethingCool --admin_user=supervisor --admin_password=password --admin_email=info@example.com
+# echo 'Installing wordpress'
+# docker-compose run --rm wp-cli wp core install --url=wordpress.develop --title=SomethingCool --admin_user=supervisor --admin_password=password --admin_email=info@example.com
 
-echo 'activate test theme'
-docker-compose run --rm wp-cli wp theme activate my-theme
+# echo 'activate test theme'
+# docker-compose run --rm wp-cli wp theme activate my-theme
 
-# echo 'Updateing database'
+# echo 'Updating URL'
 # docker-compose run --rm wp-cli wp search-replace https://$OLDURL http://$DEVURL
 
 # echo 'Activating plugins'
 # docker-compose run --rm wp-cli wp plugin activate --all
+
+# echo 'setting home page template'
+# docker-compose run --rm wp-cli wp post update 356 --page_template='templates/front-page.php'
+
+# echo 'setting up energy audit page'
+# docker-compose run --rm wp-cli wp post update 7156 --post_content='' --post_title='* Energy Audit Test' --page_template='templates/energy-audit-landing-page.php' --post_name='energy-audit-test' --post_status='publish'
+
+echo 'flushing permalinks'
+docker-compose run --rm wp-cli wp rewrite flush
