fd4747e446daae05f089cf2fe99893bc6ab59212
Jeremy Turowetz <j.turowetz@gmail.com>
2018-08-27 15:11:22 -0400

    update docker-compose-after to activate _s

--- docker-compose-after.sh
+++ docker-compose-after.sh
@@ -1,16 +1,16 @@
 #! bin/bash
 
-# echo 'Installing wordpress'
-# docker-compose run --rm wp-cli wp core install --url=wordpress.develop --title=SomethingCool --admin_user=supervisor --admin_password=password --admin_email=info@example.com
+echo 'Installing wordpress'
+docker-compose run --rm wp-cli wp core install --url=wordpress.develop --title=SomethingCool --admin_user=supervisor --admin_password=password --admin_email=info@example.com
 
-# echo 'activate test theme'
-# docker-compose run --rm wp-cli wp theme activate my-theme
+echo 'activate test theme'
+docker-compose run --rm wp-cli wp theme activate _s
 
 # echo 'Updating URL'
 # docker-compose run --rm wp-cli wp search-replace https://$OLDURL http://$DEVURL
 
-# echo 'Activating plugins'
-# docker-compose run --rm wp-cli wp plugin activate --all
+echo 'Activating plugins'
+docker-compose run --rm wp-cli wp plugin activate --all
 
 # echo 'setting home page template'
 # docker-compose run --rm wp-cli wp post update 356 --page_template='templates/front-page.php'
