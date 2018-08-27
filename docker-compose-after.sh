de0286bd2f5cfaaa79fc6f4428befbfdf19b2469
Jeremy Turowetz <j.turowetz@gmail.com>
2018-08-27 15:20:01 -0400

    replace echo with printf arguments

--- docker-compose-after.sh
+++ docker-compose-after.sh
@@ -1,22 +1,22 @@
 #! bin/bash
 
-echo 'Installing wordpress'
+printf "\n%s\n" "Installing wordpress"
 docker-compose run --rm wp-cli wp core install --url=wordpress.develop --title=SomethingCool --admin_user=supervisor --admin_password=password --admin_email=info@example.com
 
-echo 'activate test theme'
+printf "\n%s\n" "activate test theme"
 docker-compose run --rm wp-cli wp theme activate _s
 
-# echo 'Updating URL'
+# printf "\n%s\n" "Updating URL"
 # docker-compose run --rm wp-cli wp search-replace https://$OLDURL http://$DEVURL
 
-echo 'Activating plugins'
-docker-compose run --rm wp-cli wp plugin activate --all
+# printf "\n%s\n" "Activating plugins"
+# docker-compose run --rm wp-cli wp plugin activate --all
 
-# echo 'setting home page template'
+# printf "\n%s\n" "setting home page template"
 # docker-compose run --rm wp-cli wp post update 356 --page_template='templates/front-page.php'
 
-# echo 'setting up energy audit page'
+# printf "\n%s\n" "setting up energy audit page"
 # docker-compose run --rm wp-cli wp post update 7156 --post_content='' --post_title='* Energy Audit Test' --page_template='templates/energy-audit-landing-page.php' --post_name='energy-audit-test' --post_status='publish'
 
-echo 'flushing permalinks'
+printf "\n%s\n" "flushing permalinks"
 docker-compose run --rm wp-cli wp rewrite flush
