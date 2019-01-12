67a7e3fea176b3b59bde9cef5358f7ac584972a1
Jeremy Turowetz <j.turowetz@gmail.com>
2019-01-12 15:15:45 -0500

    update init scripts

--- docker-compose-after.sh
+++ docker-compose-after.sh
@@ -1,7 +1,73 @@
 #! bin/bash
 
-printf "\n%s\n" "Updating URL"
-docker-compose run --rm wp-cli wp search-replace https://www.yoursite.com http://yoursite.develop
+PROD_SITES=( yoursite.com yoursite.wpengine.com )
+SUBSITES=( blog d365 )
+OLD_CURRENT_SITE=yoursite.wpengine.com # this will get reset at some point and fuck me up
+DEV_SITE=yoursite.develop # right now the script assumes we're using non-www target domains
+
+# Go to table wp_blogs
+# Replace the domain and path fields with the new values.
+printf "\n%s\n" "Adjust the root site url to match DOMAIN_CURRENT_SITE and avoid confusion"
+docker-compose run --rm wp-cli wp search-replace ${OLD_CURRENT_SITE} ${DEV_SITE} wp_blogs wp_site
+
+# Go to table wp_options
+# In this table, change the fields site_url and home - AUTOMATIC VIA SCRIPTS BELOW
+
+# Go to table wp_sitemeta
+# Change the site_url field  - AUTOMATIC VIA SCRIPTS BELOW
+
+# wp_x_options: - AUTOMATIC VIA SCRIPTS BELOW
+#   home
+#   siteurl
+#   upload_path
+#   upload_url_path
+
+
+replace_www() {
+  local URL=$1
+  if [[ $URL == "www."* ]];
+  then
+    URL=${URL/www./}
+  fi
+  echo "$URL"
+}
+
+search_replace() {
+  local PROTOCOLS=( https:// http:// )
+  local SEARCH_FOR=$1
+  local REPLACE_WITH=$2
+  for protocol in "${PROTOCOLS[@]}"
+  do
+    printf "\n" "Updating URL from ${protocol}${SEARCH_FOR} to http://${REPLACE_WITH}"
+    docker-compose run --rm wp-cli wp search-replace ${protocol}${SEARCH_FOR} http://${REPLACE_WITH} --recurse-objects --network
+  done
+}
+
+network_search_replace() {
+  local SEARCH_FOR=$(replace_www ${1})
+  local REPLACE_WITH=$2
+
+  # runs search replace on main domain & www version if not a wpengine domain
+  search_replace ${SEARCH_FOR} ${REPLACE_WITH}
+  if [[ $SEARCH_FOR != *".wpengine."* ]];
+  then
+    search_replace www.${SEARCH_FOR} ${REPLACE_WITH}
+  fi
+
+  # runs search replace on sub domains
+  local NEW=$(replace_www ${REPLACE_WITH})
+  for slug in "${SUBSITES[@]}"
+  do
+    search_replace ${slug}.${SEARCH_FOR} ${slug}.${NEW}
+  done
+
+}
+
+for site in "${PROD_SITES[@]}"
+do
+  printf "\n%s\n" "Running search replace tasks on ${site} to ${DEV_SITE}"
+  network_search_replace ${site} ${DEV_SITE}
+done
 
 printf "\n%s\n" "flushing permalinks"
 docker-compose run --rm wp-cli wp rewrite flush
--- theme-init.sh
+++ theme-init.sh
@@ -4,11 +4,17 @@ THEME_FOLDER=./wp-content/themes/_s
 
 # Replaces strings from first array with same potision string in second array
 unset REPLACE
-declare -A REPLACE=( ["'_s'"]="'shakti'" [_s_]="shakti_" [ _s]=" Shakti" [_s-]="shakti-" [Text Domain: _s]="Text Domain: shakti" )
+declare -A REPLACE=( ["'_s'"]="'xrm'" [_s_]="xrm_" [ _s]=" xRM" [_s-]="xrm-" [Text Domain: _s]="Text Domain: xrm" )
 
 for K in "${!REPLACE[@]}"; do
   find ${THEME_FOLDER}/* -type f ! -path '*.png*' ! -path "${THEME_FOLDER}/inc/cmb2/*" -print0 | xargs -0 sed -i "s/${K}/${REPLACE[$K]}/g";
 done
+
+# Can al;so add a replcae for 2 spaces and new name to one space and new name
+
+# Can also add theme rename
+
+# can also add erase this file
 
 # This format works btw:
 # find ./wp-content/themes/_s/* -type f -not -path "./wp-content/themes/_s/inc/cmb2/*" -print0 | xargs -0 sed -i "s/${STRING}/${NEW_STRING}/g";
