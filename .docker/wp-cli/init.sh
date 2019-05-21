836a3424447bb6c9e108c916843cbdde3e085b58
Jeremy Turowetz <j.turowetz@gmail.com>
2019-05-21 16:55:54 -0400

    update init scripts with vars

--- .docker/wp-cli/init.sh
+++ .docker/wp-cli/init.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 # set -e # exit on error, even if in a while or whatever see https://www.gnu.org/software/bash/manual/bashref.html#The-Set-Builtin
 
-${REPLACE_URL:?REPLACE_URL must be defined as an enviroment variable (or an .env file for sanity). Check docker-compose.yml as this variale might be dynamically built from the PROD_URL variable}
-${WITH_URL:?WITH_URL must be defined as an enviroment variable (or an .env file for sanity). Check docker-compose.yml as this variale might be dynamically built from the DEV_URL variable}
+# >&2 echo "Install WP as ${WITH_URL:?WITH_URL must be defined as an enviroment variable (or an .env file for sanity). Check docker-compose.yml as this variale might be dynamically built from the DEV_URL variable}"
+# wp core install --url=${WITH_URL} --title="Some site" --admin_user=admin --admin_email=admin@whatever.com
 
 >&2 echo "Import DB"
 wp db import /var/www/html/.wp-cli/mysql.sql
 
->&2 echo "Updating URL"
+>&2 echo "Updating URL from ${REPLACE_URL:?REPLACE_URL must be defined as an enviroment variable (or an .env file for sanity). Check docker-compose.yml as this variale might be dynamically built from the PROD_URL variable} to ${WITH_URL:?WITH_URL must be defined as an enviroment variable (or an .env file for sanity). Check docker-compose.yml as this variale might be dynamically built from the DEV_URL variable}"
 wp search-replace ${REPLACE_URL} ${WITH_URL}
 
 # >&2 echo "Deactivate old plugins"
-# wp plugin deactivate
+# wp plugin deactivate akismet hello-dolly
 
 # >&2 echo "Delete old plugins"
 # wp plugin delete akismet hello-dolly
--- docker-compose-after.sh
+++ docker-compose-after.sh
@@ -1,14 +1,9 @@
-
 #!bin/bash
-# set -e # exit on any errors (implies linear flow)
+>&2 echo "Export environment variables from .env"
+export $(grep -v '^#' .env | xargs -0);
 
-# Check for required enviroment variables
-${INSTALLNAME:?INSTALLNAME must be defined as an enviroment variable (or an .env file for sanity)}
-${AWS_PROFILE:-default}
-
-# remove requirement for prod url in docker-compose up
->&2 echo "Set AWS_DEFAULT_PROFILE to ${AWS_PROFILE}"
-export AWS_DEFAULT_PROFILE=AWS_PROFILE
+>&2 echo "Set AWS_DEFAULT_PROFILE to ${AWS_PROFILE:-default}"
+export AWS_DEFAULT_PROFILE=${AWS_PROFILE}
 
 >&2 echo "Update composer"
 composer update
@@ -17,16 +12,16 @@ composer update
 yarn install --update --dev
 
 >&2 echo "Get remote DB"
-MSYS_NO_PATHCONV=1 docker-compose run --rm rsync rsync -avz --delete --progress -e "ssh -T -o Compression=no -x" ${INSTALLNAME}@${INSTALLNAME}.ssh.wpengine.net:sites/${INSTALLNAME}/wp-content/mysql.sql /wp-cli/
+MSYS_NO_PATHCONV=1 docker-compose run --rm rsync rsync -avz --delete --progress -e "ssh -T -o Compression=no -x" ${INSTALLNAME:?INSTALLNAME must be defined as an enviroment variable (or an .env file for sanity)}@${INSTALLNAME}.ssh.wpengine.net:sites/${INSTALLNAME}/wp-content/mysql.sql /wp-cli/
 
->&2 echo "Re-init DB and run wp-cli tasks"
+>&2 echo "Run wp-cli tasks (probably starting with a DB import)"
 MSYS_NO_PATHCONV=1 docker-compose run --rm wp-cli /var/www/html/.wp-cli/init.sh
 
 >&2 echo "Sync uploads folder"
-MSYS_NO_PATHCONV=1 docker-compose run --rm rsync rsync -avz --delete --progress --iconv=utf8,utf8 -e "ssh -T -o Compression=no -x" "${INSTALLNAME}"@"${INSTALLNAME}".ssh.wpengine.net:sites/"${INSTALLNAME}"/wp-content/uploads/ /mnt/
+MSYS_NO_PATHCONV=1 docker-compose run --rm rsync rsync -avz --delete --progress --iconv=utf8,utf8 -e "ssh -T -o Compression=no -x" "${INSTALLNAME:?INSTALLNAME must be defined as an enviroment variable (or an .env file for sanity)}"@"${INSTALLNAME}".ssh.wpengine.net:sites/"${INSTALLNAME}"/wp-content/uploads/ /mnt/
 
-#>&2 echo "Sync AWS bucket"
-# MSYS_NO_PATHCONV=1 aws s3 sync s3://largefs-"$INSTALLNAME"/"$INSTALLNAME"/wp-content/uploads/ ./wp-content/uploads/
+# >&2 echo "Sync AWS bucket"
+# MSYS_NO_PATHCONV=1 aws s3 sync s3://largefs-"${INSTALLNAME:?INSTALLNAME must be defined as an enviroment variable (or an .env file for sanity)}"/"$INSTALLNAME"/wp-content/uploads/ ./wp-content/uploads/
 
 # >&2 echo "Regen thumbnails"
 # MSYS_NO_PATHCONV=1 docker-compose run --rm wp-cli /var/www/html/.wp-cli/regen-thumbs.sh
