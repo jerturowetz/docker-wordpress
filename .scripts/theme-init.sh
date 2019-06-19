74950157589f7e0814761382afe6424fcfb57ba8
Jeremy Turowetz <j.turowetz@gmail.com>
2019-06-19 16:01:39 -0400

    imperfect search replace for _s init

--- .scripts/theme-init.sh
+++ .scripts/theme-init.sh
@@ -4,20 +4,13 @@ THEME_FOLDER=./wp-content/themes/_s
 
 # Replaces strings from first array with same potision string in second array
 unset REPLACE
-declare -A REPLACE=( ["'_s'"]="'xrm'" [_s_]="xrm_" [ _s]=" xRM" [_s-]="xrm-" [Text Domain: _s]="Text Domain: xrm" )
+declare -A REPLACE=( ["'_s'"]="'unito'" ["_s_"]="unito_" [" _s"]=" Unito" ["_s-"]="unito-" )
 
 for K in "${!REPLACE[@]}"; do
-  find ${THEME_FOLDER}/* -type f ! -path '*.png*' ! -path "${THEME_FOLDER}/inc/cmb2/*" -print0 | xargs -0 sed -i "s/${K}/${REPLACE[$K]}/g";
+  find "$THEME_FOLDER/*" -type f ! -path "*.png*" ! -path "$THEME_FOLDER/inc/cmb2/*" ! -path "*style.css" -print0 | xargs -0 sed -i "s/$K/${REPLACE[$K]}/g";
 done
 
-# Can al;so add a replcae for 2 spaces and new name to one space and new name
-
-# Can also add theme rename
-
-# can also add erase this file
-
-# This format works btw:
-# find ./wp-content/themes/_s/* -type f -not -path "./wp-content/themes/_s/inc/cmb2/*" -print0 | xargs -0 sed -i "s/${STRING}/${NEW_STRING}/g";
+# Also need to replace Text Domain: _s in style.css
 
 # Search for: '_s' and replace with: 'megatherium-is-awesome'
 # Search for: _s_ and replace with: megatherium_is_awesome_
